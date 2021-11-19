d2d79f6114547fd5dff7902ba2f1fb4fc3dde226
MemeWasTaken <memelazzari02@gmail.com>
2021-11-19 14:42:40 +0100

    problem in words

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// New km variable
+    // Ask for km
+// New age variable
+    // Ask for age
+    
+// Calc ticket price
+    // 1) 0.21 € per km
+    // 2) -20% under 18
+    // 3) -40% over 65
+// Print with 2 decimal digits
