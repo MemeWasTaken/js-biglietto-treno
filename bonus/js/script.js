3d2a1da2e6502be1bf68d902a271cfacb6a7f560
MemeWasTaken <memelazzari02@gmail.com>
2021-11-19 17:56:43 +0100

    bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,30 @@
+// New km variable
+let km;
+    // Ask for km
+    km = parseInt(prompt('Quanti kilometri devi percorrere? (Scrivere solo il numero)'));
+// New age variable
+let age;
+    // Ask for age
+    age = parseInt(prompt('Inserisci la tua età attuale:'));
+// console.log(km + ' km, ' + age + ' anni');
+
+if (!(isNaN(km)) && (km > 0) && (km != '') && !(isNaN(age)) && (age > 0) && (age != '')) {
+// Calc ticket price
+    // 1) 0.21 € per km
+    let pricePerKm = 0.21 * km;
+    // 2) -20% under 18
+    if (age < 18) {
+        pricePerKm = pricePerKm - (pricePerKm * 0.2);
+    // 3) -40% over 65
+    } else if (age > 65) {
+        pricePerKm = pricePerKm - (pricePerKm * 0.4);
+    } else {
+        pricePerKm = pricePerKm;
+    }
+console.log(pricePerKm.toFixed(2));
+// Print with 2 decimal digits
+document.writeln('Il costo del biglietto è di: '+ pricePerKm.toFixed(2) + ' €');
+
+} else {
+    alert('Ops, qualcosa è andato storto. \nRiprova e attento ad inserire solo numeri maggiori di zero.');
+}
